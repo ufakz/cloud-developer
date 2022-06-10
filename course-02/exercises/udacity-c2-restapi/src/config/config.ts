9fa10297d0a5d411ee9e3b597a7cc6b7f1135a98
Farouq Umar <ufakz99@gmail.com>
2022-06-10 23:10:03 +0100

    fix: update dependencies

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "udagram",
+    "password": "udagrampassword",
+    "database": "udagram",
+    "host": "udagram.co14cdxnp1j3.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": "us-east-1",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "udagram-farouqu"
   },
   "jwt": {
     "secret": " "
--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/models/User.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/models/User.ts
@@ -1,8 +1,8 @@
-import {Table, Column, Model, HasMany, PrimaryKey, CreatedAt, UpdatedAt} from 'sequelize-typescript';
+import { Table, Column, Model, HasMany, PrimaryKey, CreatedAt, UpdatedAt } from 'sequelize-typescript';
 
 @Table
 export class User extends Model<User> {
-  
+
   @PrimaryKey
   @Column
   public email!: string;
