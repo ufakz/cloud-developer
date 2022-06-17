4d107ef2d504143b9edacbbfbed30cb8318c1f7d
Farouq Umar <ufakz99@gmail.com>
2022-06-17 17:34:52 +0100

    feat: implement filterimage endpoint

--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -32,8 +32,8 @@ export async function filterImageFromURL(inputURL: string): Promise<string> {
 // useful to cleanup after tasks
 // INPUTS
 //    files: Array<string> an array of absolute paths to files
-export async function deleteLocalFiles(files: Array<string>) {
+export async function deleteLocalFiles(folderLoc: string, files: Array<string>) {
   for (let file of files) {
-    fs.unlinkSync(file);
+    fs.unlinkSync(`${folderLoc}/${file}`);
   }
 }
